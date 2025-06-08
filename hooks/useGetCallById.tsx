b06f504636eb5700aec92b9cc01a41091864af35
zainkhan <khanzain33999@gmail.com>
2025-06-08 12:42:57 +0500

    Revert "f"
    This reverts commit 2d904856c1dfd21064b2902bc96ff3e97a0bade6.

--- hooks/useGetCallById.tsx
+++ hooks/useGetCallById.tsx
@@ -1,7 +1,7 @@
 import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk"
 import { useState,useEffect } from "react"
 
-export const useGetCallById = (id:string|string[]|undefined)=>{
+export const useGetCallById = (id:string|string[])=>{
     const [call, setCall] = useState<Call>()
     const [isCallLoading, setIsCallLoading] = useState(true)
     const client  = useStreamVideoClient()
