8cdef4e9ae1d039f51c4575241ca152b8ff92332
zainkhan <khanzain33999@gmail.com>
2026-01-28 08:18:13 +0500

    four

--- components/ui/MeetingCard.tsx
+++ components/ui/MeetingCard.tsx
@@ -34,6 +34,7 @@ const MeetingCard = ({
     <section className="flex min-h-[258px]  w-full flex-col justify-between rounded-[14px] bg-dark1 px-5 py-8 xl:max-w-[568px]">
       <article className="flex flex-col gap-5">
         <Image src={icon} alt="upcoming" width={28} height={28} />
+        {/* //hello world */}
         <div className="flex justify-between">
           <div className="flex flex-col gap-2">
             <h1 className="text-2xl font-bold">{title}</h1>
